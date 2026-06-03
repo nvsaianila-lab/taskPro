3fb70a2037b14b0fc398123627a5c0de809ff3f2
Anila <nvsaianila@gmail.com>
2026-06-03 11:22:35 +0530

    allowed host

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-replace-me')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 # ALLOWED_HOSTS can be a comma-separated list in env, otherwise empty list
-ALLOWED_HOSTS =[*]
+ALLOWED_HOSTS =['*']
 
 
 # ================= INSTALLED APPS =================
